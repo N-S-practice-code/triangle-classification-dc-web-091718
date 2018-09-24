13b353db4d435e53d6716ac77c286df9bcddab8f
Nicholas Smith <nico.ehs05@gmail.com>
2018-09-24 14:15:46 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,9 +1,11 @@
 class Triangle
   def initialize(s1,s2,s3)
-    @sides=s1
+    @sides=[s1,s2,s3].sort
   end
   
   def kind
+    if 
+  end
  
   class TriangleError < StandardError
     # triangle error code
