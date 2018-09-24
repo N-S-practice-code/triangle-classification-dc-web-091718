1834609feca88f96ffd2d8df4feb8bc302876fdd
Nicholas Smith <nico.ehs05@gmail.com>
2018-09-24 14:24:01 +0000

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,16 @@
 class Triangle
-  # write code here
+  def initialize(s1,s2,s3)
+    @sides=[s1,s2,s3].sort
+  end
+  
+  def kind
+    raise TriangleError if @sides[0] <= 0.0 or @sides[0]+@sides[1]<=@sides[2]
+    return :equilateral if @sides[0]==@sides[1] and @sides[1]==@sides[2]
+    return :isosceles if @sides[0]==@sides[1] or @sides[1]==@sides[2]
+    :scalene
+  end
+ 
+  class TriangleError < StandardError
+    # triangle error code
+  end
 end
