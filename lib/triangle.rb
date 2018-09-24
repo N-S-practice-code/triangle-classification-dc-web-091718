d95c16889ad91dce5bb416398bc6b6cacfae0b3d
Nicholas Smith <nico.ehs05@gmail.com>
2018-09-24 14:20:16 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -6,6 +6,7 @@ class Triangle
   def kind
     raise TriangleError if @sides[0] <= 0.0 or @sides[0]+@sides[1]<@sides[2]
     return :equilateral if @sides[0]==@sides[1] and @sides[1]==@sidesp[2]
+    return :equilateral if @sides[0]==@sides[1] or @sides[1]==@sidesp[2]
     
   end
  
